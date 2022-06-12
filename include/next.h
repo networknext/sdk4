813f84eec13e8df2b0340501b008ab1d692a64ca
Glenn Fiedler <glenn@networknext.com>
2022-06-12 11:10:17 -0400

    udpate to sdk 4.20.3

--- include/next.h
+++ include/next.h
@@ -36,10 +36,10 @@
 
 #if !defined(NEXT_DEVELOPMENT)
 
-    #define NEXT_VERSION_FULL                              "4.20.2"
+    #define NEXT_VERSION_FULL                              "4.20.3"
     #define NEXT_VERSION_MAJOR_INT                                4
     #define NEXT_VERSION_MINOR_INT                               20
-    #define NEXT_VERSION_PATCH_INT                                2
+    #define NEXT_VERSION_PATCH_INT                                3
 
 #else // !defined(NEXT_DEVELOPMENT)
 
